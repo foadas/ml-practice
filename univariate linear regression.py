ad6c953ed8198ea5e8d01fe618c6320ccf2f9923
foadas <foad1381@gmail.com>
2023-07-27 22:22:02 +0430

    first algorithm

---
+++ univariate linear regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('./deeplearning.mplstyle')
+
+# the main challenge is to find right values for w and b that fit the training set data
+def compute_model_output(x, w, b):
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+
+    return f_wb
+
+
+# size of the house
+x_train = np.array([1.0, 2.0])
+# price of the house
+y_train = np.array([300.0, 500.0])
+print(f"x_train.shape: {x_train.shape}")
+print(np.zeros(2))
+tmp_f_wb = compute_model_output(x_train, 200, 100, )
+print(tmp_f_wb)
+plt.scatter(x_train, y_train, marker='x', color='r')
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+plt.show()
