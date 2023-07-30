9c0b2a0304ca6c7c62ce8b534835e0472a959795
foadas <foad1381@gmail.com>
2023-07-30 20:56:55 +0430

    plotting cost vs itreation

--- univariate linear regression.py
+++ univariate linear regression.py
@@ -3,6 +3,7 @@ import math
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 plt.style.use('./deeplearning.mplstyle')
 
 
@@ -46,7 +47,6 @@ def compute_gradient(x, y, w, b):
 
 
 def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function):
-
     w = copy.deepcopy(w_in)  # avoid modifying global w_in
     # An array to store cost J and w's at each iteration primarily for graphing later
     J_history = []
@@ -92,6 +92,16 @@ b_init = 0
 iterations = 10000
 tmp_alpha = 1.0e-2
 # run gradient descent
-w_final, b_final, J_hist, p_hist = gradient_descent(x_train ,y_train, w_init, b_init, tmp_alpha,
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train, y_train, w_init, b_init, tmp_alpha,
                                                     iterations, compute_cost, compute_gradient)
 print(f"(w,b) found by gradient descent: ({w_final:8.4f},{b_final:8.4f})")
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
+ax1.plot(J_hist[:100])
+ax2.plot(1000 + np.arange(len(J_hist[1000:])), J_hist[1000:])
+ax1.set_title("Cost vs. iteration(start)")
+ax2.set_title("Cost vs. iteration (end)")
+ax1.set_ylabel('Cost')
+ax2.set_ylabel('Cost')
+ax1.set_xlabel('iteration step')
+ax2.set_xlabel('iteration step')
+plt.show()
