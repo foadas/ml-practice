3d8e63931115a8ccb3bd005466f9c724b4b4d6b8
foadas <foad1381@gmail.com>
2023-07-30 20:33:50 +0430

    gradiant descent

--- univariate linear regression.py
+++ univariate linear regression.py
@@ -1,6 +1,8 @@
+import copy
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
-
 plt.style.use('./deeplearning.mplstyle')
 
 
@@ -26,14 +28,70 @@ def compute_cost(x, y, w, b):
     return total_cost
 
 
+def compute_gradient(x, y, w, b):
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function):
+
+    w = copy.deepcopy(w_in)  # avoid modifying global w_in
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = gradient_function(x, y, w, b)
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db
+        w = w - alpha * dj_dw
+
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            J_history.append(cost_function(x, y, w, b))
+            p_history.append([w, b])
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
+                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                  f"w: {w: 0.3e}, b:{b: 0.5e}")
+
+    return w, b, J_history, p_history  # return w and J,w history for graphing
+
+
 # size of the house
 x_train = np.array([1.0, 2.0])
 # price of the house
 y_train = np.array([300.0, 500.0])
-print(f"x_train.shape: {x_train.shape}")
-print(np.zeros(2))
-tmp_f_wb = compute_model_output(x_train, 200, 100, )
-print(tmp_f_wb)
-plt.scatter(x_train, y_train, marker='x', color='r')
-plt.plot(x_train, tmp_f_wb, c='b', label='Our Prediction')
-plt.show()
+# print(f"x_train.shape: {x_train.shape}")
+# print(np.zeros(2))
+# tmp_f_wb = compute_model_output(x_train, 200, 100, )
+# print(tmp_f_wb)
+# plt.scatter(x_train, y_train, marker='x', color='r')
+# plt.plot(x_train, tmp_f_wb, c='b', label='Our Prediction')
+# plt.show()
+w_init = 0
+b_init = 0
+# some gradient descent settings
+iterations = 10000
+tmp_alpha = 1.0e-2
+# run gradient descent
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train ,y_train, w_init, b_init, tmp_alpha,
+                                                    iterations, compute_cost, compute_gradient)
+print(f"(w,b) found by gradient descent: ({w_final:8.4f},{b_final:8.4f})")
